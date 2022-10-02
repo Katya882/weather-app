72f8bb6f7a9710c873be4a956ec0cb2902e53a66
Katya Kuripka <katyakuripkav@gmail.com>
2022-10-02 20:39:48 +0300

    add js wild and background

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 let apiKey = "5863935ee9cca4c02ed68203f807c65b";
 let tempSpan = "https://api.openweathermap.org/data/2.5/weather";
-let tempWeek ="https://api.openweathermap.org/data/2.5/forecast?";
+let tempWeek ="https://api.openweathermap.org/data/2.5/onecall?";
 
 let searchInput = document.querySelector("#cityName");
 
 let temperatureSearch = document.querySelector("#temperatureSearch");
 let iconElement = document.querySelector("#icon");
 const weatherForecastEl = document.getElementById('weather-forecast');
+const wind = document.getElementById('wind');
 
 
 function searchCity(event) {
@@ -25,33 +26,24 @@ let form = document.querySelector("#searchForm");
 form.addEventListener("submit", searchCity);
 
 function searchWeek(response){
-    let list = [];
-    response.data.list.forEach((day, idx) => {
-        let dates = day.dt_txt.slice(0,10); // 2022-09-28
+    response.data.daily.forEach((day, idx) => {
+        console.log(new Date(day.dt*1000))
 
-        if (!list[dates]) {
-            list[dates] = [];
-        }
-        list[dates].push(day);
-    })
-
-    for (let key in list){
-        var min = Math.min.apply(null, list[key].map(function(a){return a.main.temp_max;}))
-        var max = Math.max.apply(null, list[key].map(function(a){return a.main.temp_min;}))
-            weatherForecastEl.innerHTML += `
+        weatherForecastEl.innerHTML += `
             <div class="weather-forecast-item">
                 <div>
-                    <div class="day">${days[new Date(list[key][0].dt_txt).getDay()]}</div>
-                    <img src="http://openweathermap.org/img/wn//${list[key][0].weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                    <div class="day">${days[new Date(day.dt*1000).getDay()]}</div>
+                    <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
                 </div>
                 <div class="other">
-                    <div class="temp">Night - ${ Math.round(min)}&#176;C</div>
-                    <div class="temp">Day - ${ Math.round(max)}&#176;C</div>
+                    <div class="temp">Night - ${ Math.round(day.temp.night)}&#176;C</div>
+                    <div class="temp">Day - ${ Math.round(day.temp.morn)}&#176;C</div>
+                    <div class="temp">Wind - ${ Math.round(day.wind_speed)}m/c</div>
                 </div>
             </div>
             <hr>
             `
-    }
+    })
 }
 
 let now = new Date();
@@ -108,15 +100,27 @@ showCurrentTime();
 
 
 function showTemperature(response) {
-    tempCelsius.querySelector("span").innerHTML = `${Math.round(response.data.main.temp)}`
+    tempCelsius.querySelector("span").innerHTML = `${Math.round(response.data.wind.speed)}`
+    wind.querySelector("span").innerHTML = `${Math.round(response.data.main.temp)}`
     iconElement.setAttribute(
         "src",
         `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
-    console.log(response)
 
+    if(response.data.weather[0].main =="Clouds"){
+        document.querySelector('.weather').style.backgroundImage="url(src/img/cloudy.jpg)";
+    }
+    if(response.data.weather[0].main =="Rain"){
+        document.querySelector('.weather').style.backgroundImage="url(src/img/rain.jpg)";
+    }
+    if(response.data.weather[0].main =="Clear"){
+        document.querySelector('.weather').style.backgroundImage="url(src/img/sun.jpg)";
+    }
     let lat = response.data.coord.lat
     let lon = response.data.coord.lon
+    tempCelsius.querySelector("span").innerHTML = `${Math.round(response.data.main.temp)}`
+
+
     axios.get(`${tempWeek}lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`).then(searchWeek);
 
 }
